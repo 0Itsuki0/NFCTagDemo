e9cbf837d0b38c60901dfa0863c75a65a5cefbc7
itsuki <itsuki.enjyoy@gmail.com>
2024-06-16 12:31:47 +0900

    Initial Commit

---
+++ universalLinkDemo/universalLinkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  universalLinkDemoApp.swift
+//  universalLinkDemo
+//
+//  Created by Itsuki on 2024/06/16.
+//
+
+import SwiftUI
+
+@main
+struct universalLinkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
